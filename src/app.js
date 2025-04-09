27e5f6c216750860afc060d2ce42d52b9f71e51f
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-09 00:03:31 +0000

    create button with js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,17 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
 window.onload = function() {
   //write your code here
+  console.log('Project works!');
+  CreateExcuseButton();
   console.log("Hello Rigo from the console!");
 };
+
+function CreateExcuseButton(){
+  const element = document.querySelector(".alert");
+
+  element.insertAdjacentHTML('afterend', `<button class="btn btn-success" type="button">Create Excuse</button>`);
+}
